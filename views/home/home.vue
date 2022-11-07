28c0a4fac5cdbef3955eecba61802cf70edd1097
GoldenSide <1244338192@qq.com>
2022-11-07 17:56:21 +0800

    first commit

---
+++ views/home/home.vue
@@ -0,0 +1,27 @@
+<!---*
+* @description 
+* @fileName home.vue
+* @author 叶金龙
+* @date 2022/11/07 17:12:54
+*--->
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs, SetupContext, ref } from "vue";
+interface Data {}
+export default defineComponent({
+  name: "",
+  props: {},
+  components: {},
+  setup(props, ctx: SetupContext) {
+    const data: Data = reactive<Data>({});
+    return {
+      ...toRefs(data),
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss"></style>
